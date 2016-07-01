5085bfa84d74c4c9cb979056bfaebeb8d00f1960
rails <rails@rails.com>
2016-07-01 10:50:02 +0530

    first commit

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PhotoGrid = React.createClass({
+  render(){
+    return(
+      <div className='single-photo'>
+        I am a PhotoGrid
+      </div>
+    )
+  }
+})
+
+export default PhotoGrid
